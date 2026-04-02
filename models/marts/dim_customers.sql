7ef5d87b9b8073de8738fe293bd361033fd68df9
sarah siu <sarsiu@gmail.com>
2026-04-02 18:16:17 +0000

    added fct_orders table, added finance folder, added stripe folder along with stripe_payments staging data, and refactored dim_customers with lifetime value based on fct_orders.

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,7 +6,7 @@ with customers as (
 
 orders as (
 
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+    select * from {{ ref ('fct_orders') }}
     
 ),
 
@@ -15,9 +15,10 @@ customer_orders as (
     select
         customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(order_date) as first_order_date
+        , max(order_date) as most_recent_order_date
+        , count(order_id) as number_of_orders
+        , sum(amount) as lifetime_value
 
     from orders
 
@@ -29,12 +30,13 @@ customer_orders as (
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        customers.customer_id
+        , customers.first_name
+        , customers.last_name
+        , customer_orders.first_order_date
+        , customer_orders.most_recent_order_date
+        , coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        , customer_orders.lifetime_value
 
     from customers
 
