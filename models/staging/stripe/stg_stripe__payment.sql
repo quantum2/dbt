057088f4f4b83734cc23a992964c8acdb053e1b9
sarah siu <sarsiu@gmail.com>
2026-04-03 00:26:49 +0000

    refactored stg_stripe__payments.sql using source's generate model

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -9,12 +9,12 @@ source as (
 renamed as (
 
     select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status as payment_status,
-        amount as payment_amount,
-        created as payment_created
+        id,
+        orderid,
+        paymentmethod,
+        status,
+        amount,
+        created
 
     from source
 
